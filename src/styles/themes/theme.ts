e3fc5daa5ede36e65bcd60164bd1163b3d767698
PatricSPires <patric.pires@outlook.com>
2021-02-05 10:18:45 -0300

    feat: update theme and editorconfig

---
+++ src/styles/themes/theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  very_light_gray: hsl(0, 0%, 98%),
+  very_light_grayish_blue: hsl(236, 33%, 92%),
+  light_grayish_blue: hsl(233, 11%, 84%),
+  dark_grayish_blue: hsl(236, 9%, 61%),
+  very_dark_grayish_blue: hsl(235, 19%, 35%),
+}
+
+export const darkTheme = {
+  very_dark_blue: hsl(235, 21%, 11%);
+  very_dark_desaturated_blue: hsl(235, 24%, 19%),
+  light_grayish_blue: hsl(234, 39%, 85%),
+  light_grayish_blue: hsl(236, 33%, 92%), //hover
+  dark_grayish_blue: hsl(234, 11%, 52%),
+  very_dark_grayish_blue: hsl(233, 14%, 35%),
+  very_dark_grayish_blue: hsl(237, 14%, 26%),
+}
